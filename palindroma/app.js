1e51d322a7499894f992f3f0cd9be3018ecb3592
BassaniDavid <davbassan98@gmail.com>
2025-02-28 14:12:02 +0100

    feat: ask for one word

--- palindroma/app.js
+++ palindroma/app.js
@@ -2,3 +2,10 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// Chiedere all’utente di inserire una parola
+let word = prompt("inserisci una parola");
+
+// controlla che la parola sia inserita correttamente
+console.log(word)
+
+// crea una funzione
